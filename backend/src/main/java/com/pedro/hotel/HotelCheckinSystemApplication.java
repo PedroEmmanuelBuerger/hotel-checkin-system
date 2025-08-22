7a53404436ba0935992f8789c7edc52407ff6bd7
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2025-08-22 10:52:47 -0300

    iniciando projeto

---
+++ backend/src/main/java/com/pedro/hotel/HotelCheckinSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCheckinSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCheckinSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/pedro/hotel/HotelCheckinSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCheckinSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
