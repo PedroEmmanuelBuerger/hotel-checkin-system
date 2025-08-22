cdb25dc89dbcd7e889464f9e76eaed42dd5981a8
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2025-08-22 19:00:33 -0300

    substituindo banco h2 para mongo + configuração do mongodb

---
+++ scripts/init-mongo.js
@@ -0,0 +1,32 @@
+db = db.getSiblingDB('admin');
+
+
+db.createUser({
+  user: 'hotel_app',
+  pwd: 'hotel_app_password',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'hotel_checkin'
+    }
+  ]
+});
+
+
+db = db.getSiblingDB('hotel_checkin');
+
+db.createCollection('guests');
+db.createCollection('rooms');
+db.createCollection('bookings');
+db.createCollection('checkins');
+
+db.guests.createIndex({ "email": 1 }, { unique: true });
+db.guests.createIndex({ "documentNumber": 1 }, { unique: true });
+db.rooms.createIndex({ "roomNumber": 1 }, { unique: true });
+db.bookings.createIndex({ "checkInDate": 1 });
+db.bookings.createIndex({ "checkOutDate": 1 });
+db.checkins.createIndex({ "bookingId": 1 }, { unique: true });
+
+print("MongoDB inicializado com sucesso!");
+print("Usuário da aplicação criado: hotel_app");
+print("Coleções criadas: guests, rooms, bookings, checkins"); 
