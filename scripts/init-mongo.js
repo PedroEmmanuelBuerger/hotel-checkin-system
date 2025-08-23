3a22ca5ab9e4c67fe67f6785e9c5959bc1f74d71
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2025-08-23 10:51:07 -0300

    enxutando mongo removendo collections que não serão utilizadas

--- scripts/init-mongo.js
+++ scripts/init-mongo.js
@@ -13,18 +13,8 @@ db.createUser({
 
 db = db.getSiblingDB('hotel_checkin');
 
-db.createCollection('guests');
-db.createCollection('rooms');
-db.createCollection('bookings');
 db.createCollection('checkins');
 db.createCollection('pessoas');
-
-db.guests.createIndex({ "email": 1 }, { unique: true });
-db.guests.createIndex({ "documentNumber": 1 }, { unique: true });
-db.rooms.createIndex({ "roomNumber": 1 }, { unique: true });
-db.bookings.createIndex({ "checkInDate": 1 });
-db.bookings.createIndex({ "checkOutDate": 1 });
-db.checkins.createIndex({ "bookingId": 1 }, { unique: true });
 
 db.pessoas.createIndex({ "documento": 1 }, { unique: true });
 db.pessoas.createIndex({ "telefone": 1 }, { unique: true });
@@ -35,5 +25,5 @@ db.checkins.createIndex({ "pessoa.nome": 1 });
 
 print("MongoDB inicializado com sucesso!");
 print("Usuário da aplicação criado: hotel_app");
-print("Coleções criadas: guests, rooms, bookings, checkins, pessoas");
+print("Coleções criadas: checkins, pessoas");
 print("Índices criados para todas as coleções"); 
